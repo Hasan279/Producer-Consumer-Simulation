421d2cafc1fde3e292988b4ed14c160439b5bf20
Areeb Saeed <mareebsaeed@gmail.com>
2026-05-06 15:29:31 +0500

    seperate conditions

--- logger.c
+++ logger.c
@@ -6,27 +6,34 @@
 int total_produced = 0;
 int total_consumed = 0;
 
-static double get_time() {
+static double get_time()
+{
     static time_t start = 0;
-    if (start == 0) start = time(NULL);
+    if (start == 0)
+    {
+        start = time(NULL);
+    }
     return difftime(time(NULL), start);
 }
 
-void log_produced(int producer_id, int item) {
+void log_produced(int producer_id, int item)
+{
     total_produced++;
     char msg[64];
     snprintf(msg, sizeof(msg), "[%.0fs] Producer-%d inserted: %2d\n", get_time(), producer_id, item);
     log_and_print(msg);
 }
 
-void log_consumed(int consumer_id, int item) {
+void log_consumed(int consumer_id, int item)
+{
     total_consumed++;
     char msg[64];
     snprintf(msg, sizeof(msg), "[%.0fs] Consumer-%d removed:  %2d\n", get_time(), consumer_id, item);
     log_and_print(msg);
 }
 
-void print_stats() {
+void print_stats()
+{
     printf("\n=============================\n");
     printf("     Simulation Complete\n");
     printf("=============================\n");
--- main.c
+++ main.c
@@ -2,62 +2,131 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <time.h>
 #include "buffer.h"
 #include "Producer.h"
 #include "Consumer.h"
 #include "logger.h"
 
-#define NUM_PRODUCERS 3
-#define NUM_CONSUMERS 3
-#define SIM_DURATION  15  // simulation runs for 15 seconds
+// #define NUM_PRODUCERS 3
+// #define NUM_CONSUMERS 3
+#define SIM_DURATION 15
 
-int running = 1; // threads check this to know when to stop
+int running = 1;
 
-int main() {
-    printf("=== Producer-Consumer Simulation ===\n\n");
+int main()
+{
+    srand((unsigned int)time(NULL));
 
+    printf("simulations\n");
+
+    // 2 prods
+    printf("2 prods\n");
+    total_produced = 0;
+    total_consumed = 0;
+    running = 1;
     init_buffer();
 
-    // create thread arrays
-    pthread_t prod_threads[NUM_PRODUCERS];
-    pthread_t cons_threads[NUM_CONSUMERS];
+    pthread_t prod_threads_case1[2];
+    int prod_ids_case1[2] = {1, 2};
 
-    // ids so each thread knows its number
-    int prod_ids[NUM_PRODUCERS];
-    int cons_ids[NUM_CONSUMERS];
-
-    // start producer threads
-    for (int i = 0; i < NUM_PRODUCERS; i++) {
-        prod_ids[i] = i + 1;
-        pthread_create(&prod_threads[i], NULL, producer, &prod_ids[i]);
+    for (int i = 0; i < 2; i++)
+    {
+        pthread_create(&prod_threads_case1[i], NULL, producer, &prod_ids_case1[i]);
     }
 
-    // start consumer threads
-    for (int i = 0; i < NUM_CONSUMERS; i++) {
-        cons_ids[i] = i + 1;
-        pthread_create(&cons_threads[i], NULL, consumer, &cons_ids[i]);
-    }
-
-    // let simulation run
     sleep(SIM_DURATION);
-
-    // tell all threads to stop
     running = 0;
 
-    // wake up any threads stuck waiting on semaphores so they can exit
-    for (int i = 0; i < NUM_PRODUCERS + BUFFER_SIZE; i++) sem_post(&empty_slots);
-    for (int i = 0; i < NUM_CONSUMERS + BUFFER_SIZE; i++) sem_post(&full_slots);
+    for (int i = 0; i < 2 + BUFFER_SIZE; i++)
+    {
+        sem_post(&empty_slots);
+    }
+    for (int i = 0; i < BUFFER_SIZE; i++)
+    {
+        sem_post(&full_slots);
+    }
 
-    // wait for all threads to finish
-    for (int i = 0; i < NUM_PRODUCERS; i++) pthread_join(prod_threads[i], NULL);
-    for (int i = 0; i < NUM_CONSUMERS; i++) pthread_join(cons_threads[i], NULL);
+    for (int i = 0; i < 2; i++)
+    {
+        pthread_join(prod_threads_case1[i], NULL);
+    }
 
     print_stats();
-
-    // cleanup
     pthread_mutex_destroy(&mutex);
     sem_destroy(&empty_slots);
     sem_destroy(&full_slots);
+
+    // one prod one cons
+    printf("one producer one cons\n");
+    // total_produced = 0;
+    // total_consumed = 0;
+    // running = 1;
+    // init_buffer();
+    //
+    // pthread_t prod_threads_case2[1];
+    // pthread_t cons_threads_case2[1];
+    // int prod_ids_case2[1] = {1};
+    // int cons_ids_case2[1] = {1};
+    //
+    // pthread_create(&prod_threads_case2[0], NULL, producer, &prod_ids_case2[0]);
+    // pthread_create(&cons_threads_case2[0], NULL, consumer, &cons_ids_case2[0]);
+    //
+    // sleep(SIM_DURATION);
+    // running = 0;
+    //
+    // for (int i = 0; i < 1 + BUFFER_SIZE; i++)
+    // {
+    //     sem_post(&empty_slots);
+    // }
+    // for (int i = 0; i < 1 + BUFFER_SIZE; i++)
+    // {
+    //     sem_post(&full_slots);
+    // }
+    //
+    // pthread_join(prod_threads_case2[0], NULL);
+    // pthread_join(cons_threads_case2[0], NULL);
+    //
+    // print_stats();
+    // pthread_mutex_destroy(&mutex);
+    // sem_destroy(&empty_slots);
+    // sem_destroy(&full_slots);
+
+    // printf("2 cons");
+    // total_produced = 0;
+    // total_consumed = 0;
+    // running = 1;
+    // init_buffer();
+    //
+    // pthread_t cons_threads_case3[2];
+    // int cons_ids_case3[2] = {1, 2};
+    //
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     pthread_create(&cons_threads_case3[i], NULL, consumer, &cons_ids_case3[i]);
+    // }
+    //
+    // sleep(SIM_DURATION);
+    // running = 0;
+    //
+    // for (int i = 0; i < BUFFER_SIZE; i++)
+    // {
+    //     sem_post(&empty_slots);
+    // }
+    // for (int i = 0; i < 2 + BUFFER_SIZE; i++)
+    // {
+    //     sem_post(&full_slots);
+    // }
+    //
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     pthread_join(cons_threads_case3[i], NULL);
+    // }
+    //
+    // print_stats();
+    // pthread_mutex_destroy(&mutex);
+    // sem_destroy(&empty_slots);
+    // sem_destroy(&full_slots);
 
     return 0;
 }
